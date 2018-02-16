4be2b8bde7413ca75617a8022d7aee8f7e313860
TOM PLOETZ <ploetztj@miamioh.edu>
2018-02-15 19:41:24 -0500

    finished

--- app.js
+++ app.js
@@ -6,14 +6,68 @@ const descriptionButton = document.querySelector('button.description');
 const listUl = listDiv.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const lis = listUl.children;
+const firstListItem = listUl.firstElementChild;
+const lastListItem = listUl.lastElementChild;
 
+//change background color of first item and last item
+firstListItem.style.backgroundColor = 'lightskyblue';
+lastListItem.style.backgroundColor = 'lightsteelblue';
+   
+//function to add buttons to elements
+function attachListItemButtons(li) {
+    let up = document.createElement('button');
+    up.className = 'up';
+    up.textContent = 'Up';
+    li.appendChild(up);
+    
+    let down = document.createElement('button');
+    down.className = 'down';
+    down.textContent = 'Down';
+    li.appendChild(down);
+    
+    let remove = document.createElement('button');
+    remove.className = 'remove';
+    remove.textContent = 'Remove';
+    li.appendChild(remove);
+    
+}
 
+//attach buttons to elements
+for (let i = 0; i < lis.length; i += 1) {
+    attachListItemButtons(lis[i]);
+    
+}
+
+// buttons to remove list items
 listUl.addEventListener('click', (event) => {
     if (event.target.tagName == 'BUTTON') {
-       let li = event.target.parentNode;
-        let ul = li.parentNode;
-        ul.removeChild(li);
-        }  
+        if (event.target.className == 'remove') {
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+        } 
+        //move element up on click
+        if (event.target.className == 'up') {
+            let li = event.target.parentNode;
+            let prevLi = li.previousElementSibling;
+            let ul = li.parentNode;
+            //if element is on top, don't move it
+            if (prevLi){
+            ul.insertBefore(li, prevLi);
+            }
+        } 
+        //move element down on click
+        if (event.target.className == 'down') {
+            let li = event.target.parentNode;
+            let nextLi = li.nextElementSibling;
+            let ul = li.parentNode;
+            //if element is on top, don't move it
+            if (nextLi){
+            ul.insertBefore(nextLi,li);
+            }
+        } 
+    }
 });
 
 toggleList.addEventListener('click', () => {
@@ -35,9 +89,12 @@ addItemButton.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
     li.textContent = addItemInput.value;
+    attachListItemButtons(li);
     ul.appendChild(li);
     addItemInput.value = '';
 });
+
+
     
 //removeItemButton.addEventListener('click', () => {
 //    let ul = document.getElementsByTagName('ul')[0];
